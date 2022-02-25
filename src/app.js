0acc21edb1eb0af984809f625458a370ca0c6f13
AndelijaKaluderovic <andeka011@gmail.com>
2022-02-25 21:40:52 +0100

    routes + historyApiFallback

--- src/app.js
+++ src/app.js
@@ -1,6 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import 'normalize.css/normalize.css'
 import './styles/styles.scss'
-  
-ReactDOM.render(<p>This is my boilerplate</p>, document.getElementById('app'));
+
+const ExpenseDashboardPage = () => (
+        <div>
+        <p>This is from dishboard component</p>
+        </div>
+    )
+const AddExpensePage = () => (
+        <div>
+        <p>This is from add expense component</p>
+        </div>
+    )
+const EditExpensePage = () => (
+        <div>
+        <p>This is from add edit component</p>
+        </div>
+    )
+const HelpPage = () => (
+        <div>
+        <p>This is from add help component</p>
+        </div>
+    )
+
+const routes = (
+    <Router>
+        <Routes>
+        <Route path="/" element={<ExpenseDashboardPage />} exact />
+        <Route path="/create" element={<AddExpensePage />} />
+        <Route path="/edit" element={<EditExpensePage />} />
+        <Route path="/help" element={<HelpPage />} />
+        </Routes>
+    </Router>
+)
+
+ReactDOM.render(routes, document.getElementById('app'));
