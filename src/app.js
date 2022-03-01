d905f9b3cf9b1d157fc8cdb7a7690f282b3b047a
AndelijaKaluderovic <andeka011@gmail.com>
2022-03-01 18:01:25 +0100

    sortBy

--- src/app.js
+++ src/app.js
@@ -10,10 +10,9 @@ import 'normalize.css/normalize.css'
 import './styles/styles.scss'
 
 const store = configureStore();
-store.dispatch(addExpense({ description: 'Water bill', amount: 1300 }))
-store.dispatch(addExpense({ description: 'Gas bill', amount: 2600 }))
-store.dispatch(setTextFilter('bill'))
-
+store.dispatch(addExpense({ description: 'Water bill', amount: 4500 }))
+store.dispatch(addExpense({ description: 'Gas bill', createdAt: 1000 }))
+store.dispatch(addExpense({ description: 'Rent', amount: 109500 }))
 
 const state = store.getState();
 const visibleExpenses = getVisibleExpenses(state.expenses, state.filters)
