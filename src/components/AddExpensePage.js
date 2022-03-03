5cca53ee6065ca9b776bb437ca9f7326a800c7e5
AndelijaKaluderovic <andeka011@gmail.com>
2022-03-03 13:08:08 +0100

    testing filters

--- src/components/AddExpensePage.js
+++ src/components/AddExpensePage.js
@@ -3,9 +3,9 @@ import { connect } from 'react-redux';
 import { addExpense } from '../actions/expenses';
 import ExpenseForm from './ExpenseForm'
 
-class AddExpense extends React.Component {
+export class AddExpensePage extends React.Component {
     onSubmit = (expense) => {
-        this.props.onSubmit(expense);
+        this.props.addExpense(expense);
         this.props.history.push('/')
     }
     render() {
@@ -19,8 +19,9 @@ class AddExpense extends React.Component {
     }
 }
 
+// It is easier to test with mapDispatchToProps
 const mapDispatchToProps = (dispatch) => ({
-    onSubmit: (expense) => dispatch(addExpense(expense))
+    addExpense: (expense) => dispatch(addExpense(expense))
 });
 
-export default connect(undefined, mapDispatchToProps)(AddExpense)
+export default connect(undefined, mapDispatchToProps)(AddExpensePage)
--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Header from '../components/Header'
-import AddExpense from '../components/AddExpense'
+import AddExpensePage from '../components/AddExpensePage'
 import Dashboard from '../components/Dashboard'
 import EditExpense from '../components/EditExpense'
 import Help from '../components/Help'
@@ -14,7 +14,7 @@ const AppRouter = () => (
             <Header />
             <Switch>
                 <Route path="/" component={Dashboard} exact={true} />
-                <Route path="/create" component={AddExpense} />
+                <Route path="/create" component={AddExpensePage} />
                 <Route path="/edit/:id" component={EditExpense} />
                 <Route path="/help" component={Help} />
                 <Route component={NotFound} />
