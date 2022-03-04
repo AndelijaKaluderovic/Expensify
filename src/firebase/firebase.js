2718ebe6c165529a50206219937b10bec8a5bc1f
AndelijaKaluderovic <andeka011@gmail.com>
2022-03-04 18:50:04 +0100

    setup test database env

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,16 +1,16 @@
 import * as firebase from 'firebase';
 
-const firebaseConfig = {
-    apiKey: "AIzaSyCy5oBUUYXJme964bg8a-C5458b15WknNY",
-    authDomain: "expensify-app-342ef.firebaseapp.com",
-    databaseURL: "https://expensify-app-342ef-default-rtdb.firebaseio.com",
-    projectId: "expensify-app-342ef",
-    storageBucket: "expensify-app-342ef.appspot.com",
-    messagingSenderId: "861061885339",
-    appId: "1:861061885339:web:6938cbc1764f16dd5cc53e",
-    measurementId: "G-PSX43P9DJ0"
+const config = {
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID,
+    measurementId: process.env.FIREBASE_MEASUREMENT_ID
   };
-  firebase.initializeApp(firebaseConfig);
+  firebase.initializeApp(config);
 
   const database = firebase.database();
 
--- src/tests/setupTests.js
+++ src/tests/setupTests.js
@@ -1,7 +1,8 @@
 import Enzyme from 'enzyme';
 import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+require('dotenv').config({ path: '.env.test' });
+
 // yarn add enzyme raf @wojtekmaj/enzyme-adapter-react-17 TERMINAL
- 
 Enzyme.configure({ 
     adapter: new Adapter()
 });
