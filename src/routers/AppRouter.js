c9a6432d506766dee56f1cc9e4b3ac0e94a61e53
AndelijaKaluderovic <andeka011@gmail.com>
2022-03-07 13:35:11 +0100

    log in and lof out functionality

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,11 +1,12 @@
-import React from 'react'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import Header from '../components/Header'
-import AddExpensePage from '../components/AddExpensePage'
-import Dashboard from '../components/Dashboard'
-import EditExpense from '../components/EditExpense'
-import Help from '../components/Help'
-import NotFound from '../components/NotFound'
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Header from '../components/Header';
+import LoginPage from '../components/LoginPage';
+import AddExpensePage from '../components/AddExpensePage';
+import Dashboard from '../components/Dashboard';
+import EditExpense from '../components/EditExpense';
+import Help from '../components/Help';
+import NotFound from '../components/NotFound';
 
 
 const AppRouter = () => (
@@ -13,7 +14,8 @@ const AppRouter = () => (
         <div>
             <Header />
             <Switch>
-                <Route path="/" component={Dashboard} exact={true} />
+                <Route path="/" component={LoginPage} exact={true} />
+                <Route path="/dashboard" component={Dashboard}/>
                 <Route path="/create" component={AddExpensePage} />
                 <Route path="/edit/:id" component={EditExpense} />
                 <Route path="/help" component={Help} />
