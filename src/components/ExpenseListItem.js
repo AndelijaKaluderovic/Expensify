bdc45d92b4e126a7c19833907184f917f69fcdf3
AndelijaKaluderovic <andeka011@gmail.com>
2022-03-08 23:00:40 +0100

    v1 done

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -3,14 +3,15 @@ import { Link } from 'react-router-dom';
 import moment from 'moment';
 import numeral from 'numeral';
 
-const ExpenseListItem = ({ id, description, amount, createdAt }) => (
+const ExpenseListItem = ({ id, description, note, amount, createdAt }) => (
     <Link className='list-item' to={`/edit/${id}`}>
         <div>
-            <h3 className='list-item__title'>{description}</h3>
+            <h3 className='list-item__description'>{description}</h3>
             <span>{moment(createdAt).format('MMMM Do, YYYY')}</span>
+            {note && <p className='list-item__note'>Note: {note}</p>}
 
         </div>
-        <h3 className='list-item__sub-title'>
+        <h3 className='list-item__amount'>
             {numeral(amount / 100).format('$0,0.00')}
         </h3>
     </Link>
